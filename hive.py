996db36570f9e56dd5713fca5037a0bb0424a58f
anselmrothe <anselmrothe@users.noreply.github.com>
2018-10-15 13:46:25 -0400

    webhook (doesn’t work)

--- hive.py
+++ hive.py
@@ -1,5 +1,8 @@
+from github_webhook import Webhook
 from flask import Flask
+
 app = Flask('hive', static_folder='site', static_url_path='')
+webhook = Webhook(app)
 
 
 @app.route('/')
@@ -11,6 +14,12 @@ def show_main():
 def show_html(variable):
     return app.send_static_file('{}/index.html'.format(variable))
 
+
+# Define a handler for the 'push' event
+@webhook.hook()
+def on_push(data):
+    print("Got push with: {0}".format(data))
+
 # run the application with debug mode
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')

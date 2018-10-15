a02b9ea1c48e8282af73070dcce6f7bb86b01a59
anselmrothe <anselmrothe@users.noreply.github.com>
2018-10-15 13:57:14 -0400

    run locally

--- hive.py
+++ hive.py
@@ -22,4 +22,5 @@ def on_push(data):
 
 # run the application with debug mode
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True)
